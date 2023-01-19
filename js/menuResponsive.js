6cba06527bcae50807939cbd4a5961a72369d6a9
Ricardo Dias <96152954+ricardond@users.noreply.github.com>
2023-01-19 16:25:58 -0300

    Atualização da responsividade

---
+++ js/menuResponsive.js
@@ -0,0 +1,8 @@
+/* Quando o usuário clica no Ícone, abre novamente o Menu só que desta vez com a formatação que definimos no media screen */
+function myFunction() {
+    document.getElementsByClassName("menus")[0].classList.toggle("responsive");
+}
+
+/*
+OBS: Esta função deverá ser adicionada dentro de um Script na sua página HTML, ou dentro de uma chama de arquivo .js
+*/
