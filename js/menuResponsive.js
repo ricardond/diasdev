9c91ddbab628269966fe7d968d2f8f05d575b0ac
Ricardo Dias <96152954+ricardond@users.noreply.github.com>
2023-01-20 09:41:24 -0300

    Atualização no javascript

--- js/menuResponsive.js
+++ js/menuResponsive.js
@@ -6,3 +6,8 @@ function myFunction() {
 /*
 OBS: Esta função deverá ser adicionada dentro de um Script na sua página HTML, ou dentro de uma chama de arquivo .js
 */
+
+window.onscroll = () => {
+    const nav = document.querySelector('header');
+    if(this.scrollY > 100) nav.className = 'menuFixo'; else nav.className = 'menu';
+  };
